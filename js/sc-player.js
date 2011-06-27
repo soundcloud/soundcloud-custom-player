96fd42ac6ab7b39c074656cb1218f1e6ecf4f711
Matas <matas@soundcloud.com>
2011-06-27 12:12:38 +0200

    regular expression in stream signing fixed

--- js/sc-player.js
+++ js/sc-player.js
@@ -53,7 +53,7 @@
         return (/api\./.test(url) ? url + '?' : 'http://api.' + domain +'/resolve?url=' + url + '&') + 'format=json&consumer_key=' + apiKey +'&callback=?';
       };
 
-
+  // TODO Expose the audio engine, so it can be unit-tested
   var audioEngine = function() {
     var html5AudioAvailable = function() {
         var state = false;
@@ -116,7 +116,7 @@
       return {
         load: function(track, apiKey) {
           player.pause();
-          player.src = track.stream_url + (/\\?/.test(track.stream_url) ? '&' : '?') + 'consumer_key=' + apiKey;
+          player.src = track.stream_url + (/\?/.test(track.stream_url) ? '&' : '?') + 'consumer_key=' + apiKey;
           player.load();
           player.play();
         },
